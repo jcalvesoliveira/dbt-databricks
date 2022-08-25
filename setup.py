286c0787b98c429a9c8cddb38a73b98b670200bc
Julio Oliveira <jcalvesoliveira@hotmail.com>
2022-08-25 16:50:19 -0400

    updating dbt-spark version

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ if sys.version_info < (3, 7):
     print("Please upgrade to Python 3.7 or higher.")
     sys.exit(1)
 
-
 # require version of setuptools that supports find_namespace_packages
 from setuptools import setup
 
@@ -17,30 +16,34 @@ try:
 except ImportError:
     # the user has a downlevel version of setuptools.
     print("Error: dbt requires setuptools v40.1.0 or higher.")
-    print('Please upgrade setuptools with "pip install --upgrade setuptools" and try again')
+    print(
+        'Please upgrade setuptools with "pip install --upgrade setuptools" and try again'
+    )
     sys.exit(1)
-
 
 # pull long description from README
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md"), "r", encoding="utf8") as f:
+with open(os.path.join(this_directory, "README.md"), "r",
+          encoding="utf8") as f:
     long_description = f.read()
 
 
 # get this package's version from dbt/adapters/<name>/__version__.py
 def _get_plugin_version():
-    _version_path = os.path.join(this_directory, "dbt", "adapters", "databricks", "__version__.py")
+    _version_path = os.path.join(this_directory, "dbt", "adapters",
+                                 "databricks", "__version__.py")
     try:
         exec(open(_version_path).read())
         return locals()["version"]
     except IOError:
-        print("Failed to load dbt-databricks version file for packaging.", file=sys.stderr)
+        print("Failed to load dbt-databricks version file for packaging.",
+              file=sys.stderr)
         sys.exit(-1)
 
 
 package_name = "dbt-databricks"
 package_version = _get_plugin_version()
-dbt_spark_version = "1.3.0a1"
+dbt_spark_version = "1.3.0b1"
 description = """The Databricks adapter plugin for dbt"""
 
 setup(
